0717c009e1b7b925b3a6371f866ee47e0659c5c0
hubert-kniola <hubert.kniola@gmail.com>
2021-08-26 14:40:37 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { lazy, Suspense } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
